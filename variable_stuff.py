3960cd7eba240d5441606ed6de4324355f3f6fd9
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-19 09:45:54 -0600

    first notes python page

---
+++ variable_stuff.py
@@ -0,0 +1,31 @@
+# Judah Beagley, notes mainly
+#variable example
+name = "katie"
+
+input("what is your name: ")
+print("well im just going to call you " +name+ " because im lazy.")
+
+# another variable example
+age = 14
+
+print("wow you are", age)
+print("oh wait your birthday was yesterday?")
+age += 1
+print("so you are", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age += 1
+print("or are you", age)
+age -= 7
+print("or are you", age)
+print("oh well i wont dig into it.")
