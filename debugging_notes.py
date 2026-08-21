bf5225fa7e6738130dac27cfb325f92f3116aaa6
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-21 09:53:09 -0600

    debugging notes

---
+++ debugging_notes.py
@@ -0,0 +1,22 @@
+# Judah Beagley, debugging NOTES
+
+# Syntax error
+print("hello world")
+
+#indentation error
+
+
+people = 10
+print(people)
+
+#logic error
+apples = 20
+people = 3
+# read the code again pal.
+print(apples * people)
+
+# run time errors
+fav_block = input("what. what is your favorite block in minecraft? ")
+while (fav_block):
+    isinstance
+    print("what is your favorite block in minecraft")
