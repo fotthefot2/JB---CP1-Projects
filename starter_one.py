6c1223725005e5d931b17341eaa0778195070aa8
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-21 09:05:36 -0600

    starter 1

---
+++ starter_one.py
@@ -0,0 +1,30 @@
+#starter judah beagley
+# calculate area and perimeter of a rectangle
+
+# rectangle dimensions
+length = 5
+width = 3
+
+
+# calculate area
+area= length * width
+
+
+# calculate permimeter
+perimeter = 2 * (length + width)
+
+
+# print results
+print("rectangle dimensions:")
+
+print("length:", length)
+
+print("width:", width)
+
+print("area of the rectangle:", area)
+
+print("perimeter of the rectangle:", perimeter)
+
+# convert to square feet
+square_feet = area * 10.764
+print("area in square feet:", square_feet)
