f64a6d1f6faad7e6d31dc2cb32056d53377b7ca6
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-24 09:13:20 -0600

    forgot comments for debugging assignment

--- debugging.py
+++ debugging.py
@@ -1,5 +1,6 @@
 # Judah Beagley, debugging
 name = "Alex"
 age = 16
-
+# no closing parenthenses at the end
+# name  was spelt "nam"
 print("My name is " + name + " and I am " + str(age) + " years old.")
