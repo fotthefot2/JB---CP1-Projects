b60b0cc2f247562fc68ad1c9c9a29175af3c9256
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-26 09:22:48 -0600

    unit 1 final project submission

---
+++ tic_tac_toe.py
@@ -0,0 +1,32 @@
+# Judah Beagley, Tic Tac Toe for fun
+#im really curious if i could make tic tac toe with some help but my ideas
+
+#how the board looks
+print("this is how the board looks for tic tac toe.")
+print("3")
+print("2")
+print("1")
+print("  A  B  C")
+# transition/rules
+user_input = input("do you know the rules: ")
+
+if user_input == "no":
+    print("you need to type a combination of words in order EX:A1.")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(2)
+    print("then there should be an x in that spot and then the next player plays.")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(3)
+    print("there is no bot im not making a bot for this.")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(2)
+    print("the next player does the same and it should appear as an O.")
+else:
+    print("ok")
+# help me this is annoying to write.
---
+++ unit1_final_project.py
@@ -0,0 +1,36 @@
+#Judah Beagley, Unit 1 Final Project
+
+#introduction
+#code should ask name, age, what school you currently go to or what school you last went to, and your favorite band
+
+#name
+name=input("what is your name: ")
+#age
+age=input("how old are you: ")
+#school
+school=input("what school do you currently go to or last went: ")
+#band
+band=input("and what would be your favorite band: ")
+
+#time to use the inputs we just made
+
+# name and age goes here
+print("Well hello " +name+ "! " +age+ " is really the best age.")
+import time
+
+for delay in():
+    print(delay)
+time.sleep(3)
+#school code. if the user puts ucas as their answer the terminal will say they also go there
+if school ==("ucas"):
+    print("I'm also a " +school+ " student!")
+else:
+    print("i've never been to " +school+ ", but ive heard good things about it.")
+import time
+
+for delay in():
+    print(delay)
+time.sleep(2)
+
+# the band they went to.
+print(band+" has some pretty good songs! good choice.")
