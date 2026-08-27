1727a3ebb7a5e90619b363f8ff4942cedb0cfd81
UCAS-fotthefot2 <judah.beagley@ucas-edu.net>
2026-08-27 09:16:51 -0600

    edits to make final project look better

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -3,34 +3,54 @@
 #introduction
 #code should ask name, age, what school you currently go to or what school you last went to, and your favorite band
 
-#name
-name=input("what is your name: ")
-#age
-age=input("how old are you: ")
-#school
-school=input("what school do you currently go to or last went: ")
-#band
-band=input("and what would be your favorite band: ")
+#the bot intruduces itself
+print("hello there! my name is judah and i want to ask some questions about you.")
+#bot makes sure they are fine with disclosing info about the user
+okay=input("is that okay? ")
+if okay == ("yes"):
+        
+    print("okay perfect!")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(2)
+    #name
+    name=input("what is your name: ")
+    print("Well hello " +name+ "!")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(2)
+    #age
+    age=input("how old are you: ")
+    print(age+"really is the best age.")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(2)
+    #school
+    school=input("what school do you currently go to or last went: ")
+    #if the user puts ucas as their answer the terminal will say they also go there
+    if school ==("ucas"):
+        print("I'm also a " +school+ " student!")
+    else:
+        print("i've never been to " +school+ ", but ive heard good things about it.")
+    import time
+    for delay in():
+        print(delay)
+    time.sleep(3)
 
-#time to use the inputs we just made
+    #band
+    band=input("and what would be your favorite band: ")
+    print(band+" has some pretty good songs! good choice.")
 
-# name and age goes here
-print("Well hello " +name+ "! " +age+ " is really the best age.")
-import time
 
-for delay in():
-    print(delay)
-time.sleep(3)
-#school code. if the user puts ucas as their answer the terminal will say they also go there
-if school ==("ucas"):
-    print("I'm also a " +school+ " student!")
+
+
+
+
+
+
+    #if they said no.
 else:
-    print("i've never been to " +school+ ", but ive heard good things about it.")
-import time
-
-for delay in():
-    print(delay)
-time.sleep(2)
-
-# the band they went to.
-print(band+" has some pretty good songs! good choice.")
+    print("well now you dont get to see my beautiful code!")
