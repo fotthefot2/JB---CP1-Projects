8ed013471dae52f11f34c881de9ec76498f5c4db
fotthefot2 <judah.beagley@ucas-edu.net>
2026-09-03 09:53:02 -0600

    madlib assignment

---
+++ games/madlib.py
@@ -0,0 +1,25 @@
+# Judah Beagley, period 1, madlib assignment
+#  I, MOVIE CHARACTER NAME, choose you, TV CHARACTER NAME, to be my spouse for life. Together, we’ll face the ups and PLURAL DIRECTION, always by each other’s side. I offer you my BODY PART and BODY ORGAN as a safe haven filled with love and NOUN. 
+# I promise to stay faithful and devoted to you. Like this never-ending SHAPE, my love for you will endure AMOUNT OF TIME. Just as this ring is made of ADJECTIVE material, my commitment to you will never VERB. With this ring, I VERB you.
+
+#inputs
+
+movie = input ("MOVIE CHARACTER NAME: ")
+television = input ("TV CHARACTER NAME: ")
+direction = input ("PLURAL DIRECTION: ")
+body_part = input ("BODY PART: ")
+body_organ = input ("BODY ORGAN: ")
+first_noun = input ("NOUN: ")
+shape = input ("SHAPE: ")
+time = input ("AMOUNT OF TIME: ")
+first_adjective = input ("ADJECTIVE: ")
+first_verb = input ("VERB: ")
+second_verb = input ("VERB: ")
+answer = input ("YES OR NO: ")
+
+#using the inputs
+
+print("I, " +movie+ " choose you, " +television+ " to be my spouse for life. Together, we'll face the ups and " +direction+ ", always by each other's side. I offer you my " +body_part+ " and " +body_organ+ " as a safe haven filled with love and " +first_noun+ ".")
+
+print("I promise to stay faithful and devoted to you. Like this never-ending " +shape+ ", my love for you will endure " +time+ ". Just as this ring is made of" +first_adjective+ " material, my commitment to you will never " +first_verb+ ". With this ring, i " +second_verb+ " you.")
+print("will you, " +television+ " make me, " +movie+ " the happiest man alive and say " +answer+ "?")
