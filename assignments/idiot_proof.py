c6a71302c2f90f7a126da3a83d4f89bf34addd64
fotthefot2 <judah.beagley@ucas-edu.net>
2026-09-08 09:53:33 -0600

    organizing and some work on idiot_proof

---
+++ assignments/idiot_proof.py
@@ -0,0 +1,13 @@
+#Judah Beagley, idiot proof assignment
+
+# name inputs
+
+first_name = input("what is your first name: ").strip().split()
+last_name = input("what is your last name: ").strip().split()
+
+# full name
+full_name ="".join(first_name).title() + "".join(last_name).title()
+
+phone_number = input("what is your phone number? if you dont have a number type 0: ").strip().split()
+
+
