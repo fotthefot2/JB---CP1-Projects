70db70538309c2eb68a1a4bf85e31648788ce095
fotthefot2 <judah.beagley@ucas-edu.net>
2026-09-09 09:53:30 -0600

    almost done with idiot_proof

--- assignments/idiot_proof.py
+++ assignments/idiot_proof.py
@@ -2,12 +2,36 @@
 
 # name inputs
 
-first_name = input("what is your first name: ").strip().split()
-last_name = input("what is your last name: ").strip().split()
-
-# full name
-full_name ="".join(first_name).title() + "".join(last_name).title()
-
-phone_number = input("what is your phone number? if you dont have a number type 0: ").strip().split()
+first_name = input("What is your first name: ").strip().split()
+last_name = input("What is your last name: ").strip().split()
 
 
+#phone number input and checker to see if user has phone number
+while True:
+    try:       
+        phone_number = int(input("What is your phone number? if you dont have a number type 0: ").strip())
+
+        break
+    except ValueError:
+        print("What is not a valid phone number")
+if phone_number == ("0"):
+    phone_number = False
+
+while True:
+    try:
+        gpa = float(input("What is your GPA: "))
+        if 0 <= gpa <= 5:
+            break
+        else:
+            print("that is not a valid GPA")
+    except ValueError:
+        print("that is not a valid GPA")
+
+if phone_number == False:
+    print(f"Name= {first_name} {last_name}")
+    print(f"you have no phone number")
+    print(f"GPA= {gpa}")
+else:
+    print(f"Name= {first_name} {last_name}")
+    print(f"phone number= {phone_number}")
+    print(f"GPA= {gpa}")
---
+++ notes/using_random.py
@@ -0,0 +1,9 @@
+import random
+
+ducks = random.randint(1,10)
+
+print(f"there are {ducks} ducks!")
+
+pens = random.randrange(2,10,2)
+
+print(f"i have and even number of {pens} pens.")
