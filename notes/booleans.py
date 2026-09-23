b54aa5999a6fc16e83d4a34ee5a66432d27f2fe5
fotthefot2 <judah.beagley@ucas-edu.net>
2026-09-23 09:46:36 -0600

    boolean notes

---
+++ notes/booleans.py
@@ -0,0 +1,15 @@
+#judah beagley, booleans notes
+
+#booleans are trues and falses. they are pretty cool
+
+win = False
+
+admin = True
+age = 18
+if 18 < age:
+    print("you an adult type stuff")
+
+
+#simple types are booleans, strings, data types, etc
+
+print(bool(age))
